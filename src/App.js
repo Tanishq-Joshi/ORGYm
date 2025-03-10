f1d97737ddf2309a92d3df587a9039a0c787eddd
Tanishq Joshi <113236940+Tanishq-Joshi@users.noreply.github.com>
2025-03-10 16:04:32 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ExerciseDetails from './pages/ExerciseDetails';
@@ -15,6 +17,8 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/exercise/:id" element={<ExerciseDetails/>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
             </Routes>
             <Footer />
         </Box>
