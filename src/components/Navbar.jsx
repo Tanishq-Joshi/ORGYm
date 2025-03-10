35f4b8bfeebeba50d21684df457372259e854292
Tanishq Joshi <113236940+Tanishq-Joshi@users.noreply.github.com>
2025-03-10 16:15:40 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,10 +18,9 @@ const Navbar = () => (
         >
             <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
             <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
-            <ul>
-        <li><Link to="/login" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Login</Link></li>
-        <li><Link to="/signup" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Signup</Link></li>
-      </ul>
+            <Link to="/login" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Login</Link>
+            <Link to="/signup" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Signup</Link>
+      
         </Stack>
     </Stack>
 );
