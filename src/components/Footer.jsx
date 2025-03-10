6e15539aa1889e7cc9926da2311528bf5fd6a0ae
Tanishq Joshi <113236940+Tanishq-Joshi@users.noreply.github.com>
2025-03-10 15:39:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <img src={Logo} alt="logo" style={{ width: '100px', height: '70px' }} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made by Tanishq Joshi(RA2211003010441) & Bharat Malhotra(RA2211003010) </Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made by Tanishq Joshi(RA2211003010441), Shivam Sharma(RA2211003010444), Priya(RA2211003010445), Ayush Raj(RA2211003010447) </Typography>
   </Box>
 );
 
