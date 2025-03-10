493aed825ba343a2f81618ded2de60a71efc71dd
Tanishq Joshi <113236940+Tanishq-Joshi@users.noreply.github.com>
2025-03-10 16:17:15 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => (
             alignItems="flex-end"
         >
             <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-            <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+            <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Exercises</a>
             <Link to="/login" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Login</Link>
             <Link to="/signup" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Signup</Link>
       
